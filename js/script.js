8f96019137a0044d51eb7b1252a4fe603fdfd90b
Ketan Goel <ketangoel03@gmail.com>
2023-09-06 15:29:31 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Typing effect from typed js
+
+var typed = new Typed(".auto-type", {
+    strings: ["CAD Services", "GIS Solutions", "Solar Designs", "Telecom Services"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+//loading
+
